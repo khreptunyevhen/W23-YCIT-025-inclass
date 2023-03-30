203ff83d41db7adab99ea1b5149ae82b6ee7ac65
Yevhen Khreptun <khreptun.e@gmail.com>
2023-03-30 19:02:07 -0300

    did 02-03 challenges about ==, unary+ and concat

---
+++ session_03/challenge_02.js
@@ -0,0 +1,11 @@
+const var1 = 9;
+const var2 = [9];
+
+console.log(var1);
+console.log(var2);
+
+console.log(typeof var1);
+console.log(typeof var2);
+
+console.log(var1 == var2);
+console.log(var1 === var2);
---
+++ session_03/challenge_03.js
@@ -0,0 +1,8 @@
+console.log({} + []);
+
+console.log(+[]); // 0
+console.log(+{}); // NaN
+console.log(+"1");
+console.log(+"1.4545");
+console.log(+"$1.4545");
+console.log(+"1.4545$");
